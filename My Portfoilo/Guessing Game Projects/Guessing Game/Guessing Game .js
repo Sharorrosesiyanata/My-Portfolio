b091c8638ec529791294c60906e57c4c8255501c
Sharon-Rose Siyanata <sharonrosesiyanata7@gmail.com>
2024-05-24 12:50:21 +0200

    Add files via upload

---
+++ My Portfoilo/Guessing Game Projects/Guessing Game/Guessing Game .js
@@ -0,0 +1,40 @@
+/*
+let secret = 7;
+let answer = prompt (“Please guess the secret number (1-20)”);
+// Convert the string guess to an integer so that we can compare
+var guess = parseInt (answer);
+
+Please add an if statement which checks if the guess is correct, if it is, then display a message: Correct Guess!, otherwise “Sorry, incorrect Guess!
+How can you combine line 2 and 3 into one line
+Expand the program to do this in a while loop, only exiting when the guess was correct
+While the guess is incorrect, test also if it is too low or too high and display the message “Incorrect, too low” or “Incorrect, too high”.
+When you are done make the secret number random.
+*/
+
+let secret = 5;
+
+
+/*
+if (guess === 7) {
+    alert("Correct!")
+} else {
+    alert("Sorry, incorrect guess")
+}
+*/
+while (parseInt(guess) !== 7) {
+    var Guess = alert("Hey Sharon, Guess a secret number between 1 && 20.");
+    var guess = prompt("Enter the secret number");
+    if (guess === null) {
+        alert("You canceled but you can do this. Lets try again ")
+        break;
+    }
+    if (guess == secret) {
+        alert("Correct guess!");
+        break;
+    } else if (guess < secret) {
+        alert("Incorrect guess! Too low")
+    } else if (guess > secret) {
+        alert("Incorrect guess! Too high")
+    }
+}
+
